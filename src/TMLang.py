b088c4b3daf88a91acf8e3561ccd01a314937eca
Timothée Monin <timothee@monin.net>
2025-01-26 17:08:06 +0100

    Put cli interface inside a main function

--- src/TMLang.py
+++ src/TMLang.py
@@ -3,49 +3,55 @@ from TMLang_interpreter import interpret_from_code, TMLangSyntaxError, TMLangVal
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(
-    description="A small language for designing and programming Turing machines",
-)
 
-parser.add_argument("filename", help="the path to the file containing the code to interpret")
+def main():
+    parser = argparse.ArgumentParser(
+        description="A small language for designing and programming Turing machines",
+    )
 
-parser.add_argument(
-    "-o",
-    "--output",
-    default=None,
-    help="the file to write the generated output (default: stdout)",
-)
+    parser.add_argument("filename", help="the path to the file containing the code to interpret")
 
-parser.add_argument(
-    "-v",
-    "--verify",
-    action="store_true",
-    help="if used, the output is printed only in case of error in the program given",
-)
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=None,
+        help="the file to write the generated output (default: stdout)",
+    )
 
-parser.add_argument(
-    "-a",
-    "--auto-open",
-    action="store_true",
-    help="when rendering a transition diagram, the program will automatically the generated image",
-)
+    parser.add_argument(
+        "-v",
+        "--verify",
+        action="store_true",
+        help="if used, the output is printed only in case of error in the program given",
+    )
 
-args = parser.parse_args()
+    parser.add_argument(
+        "-a",
+        "--auto-open",
+        action="store_true",
+        help="when rendering a transition diagram, the program will automatically the generated image",
+    )
 
-with open(args.filename, "r") as file:
-    code = file.read()
+    args = parser.parse_args()
 
-if args.output is not None:
-    sys.stdout = open(args.output, "w", encoding="utf-8")
+    with open(args.filename, "r") as file:
+        code = file.read()
 
-try:
-    for i in interpret_from_code(code, automatically_open_image_generated=args.auto_open):
-        if not args.verify:
-            print(i)
+    if args.output is not None:
+        sys.stdout = open(args.output, "w", encoding="utf-8")
 
-except TMLangSyntaxError as error:
-    print(f"TMLangSyntaxError: {error}")
-except TMLangValueError as error:
-    print(f"TMLangValueError: {error}")
-except TuringMachineError as error:
-    print(f"TuringMachineError: {error}")
+    try:
+        for i in interpret_from_code(code, automatically_open_image_generated=args.auto_open):
+            if not args.verify:
+                print(i)
+
+    except TMLangSyntaxError as error:
+        print(f"TMLangSyntaxError: {error}")
+    except TMLangValueError as error:
+        print(f"TMLangValueError: {error}")
+    except TuringMachineError as error:
+        print(f"TuringMachineError: {error}")
+
+
+if __name__ == "__main__":
+    main()
